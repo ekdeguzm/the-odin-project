bc7a8b23ff39df171136b515fa70ac0b77725068
Kawayan <kawayan.deguzman@gmail.com>
2023-11-25 18:52:44 -0800

    import myName

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,24 @@
-import _ from 'lodash'
+// import _ from 'lodash'
+import myName from './myName';
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  // use your function!
+  element.textContent = myName('Cody');
   return element;
 }
 
 document.body.appendChild(component());
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
 
