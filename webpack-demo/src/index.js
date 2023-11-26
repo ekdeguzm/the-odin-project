ff9ca8794314d8b312af0d4a5e005aa6c8734aed
Kawayan <kawayan.deguzman@gmail.com>
2023-11-25 19:06:04 -0800

    import style.css

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,24 +1,27 @@
-// import _ from 'lodash'
+import _ from 'lodash'
 import myName from './myName';
-
-function component() {
-  const element = document.createElement('div');
-
-  // use your function!
-  element.textContent = myName('Cody');
-  return element;
-}
-
-document.body.appendChild(component());
+import './style.css';
 
 // function component() {
 //   const element = document.createElement('div');
 
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+//   // use your function!
+//   element.textContent = myName('Cody');
 //   return element;
 // }
 
-// document.body.appendChild(component());
+document.body.appendChild(component());
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+
+  return element;
+}
+
+document.body.appendChild(component());
 
